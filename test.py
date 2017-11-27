29445d46579cfd4ce34daa3f30cfc569db820828
Kyle1668 <kyledevinobrien1@gmail.com>
2017-11-27 14:09:35 -0800

    init

---
+++ test.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+
+def return_get(location_name):
+    return "https://maps.googleapis.com/maps/api/place/textsearch/json?query=Cafe+coffee+near+" \
+          + location_name + "&key=AIzaSyC_KZyErDtZ42CuFscO2l5YseWaV8MCHrQ&sensor=false"
+
+
+def places_api_request(request_url):
+    data = urllib.request.urlopen(request_url).reads()
+    return json.load(data)
+
+
+def main():
+    location_name = input("Enter Location Name: ")
+    request_url = return_get(location_name)
+    json_data = places_api_request(request_url)
+
+    for cafe in json_data["results"]:
+        print(cafe["formatted_address"])
+
+main()
