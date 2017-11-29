21425b04d1bbc63dfb4feac11a63003da457b650
Kyle1668 <kyledevinobrien1@gmail.com>
2017-11-28 22:34:54 -0800

    Commented out inproper fields

--- place.py
+++ place.py
@@ -2,15 +2,18 @@ class Place:
 
     def __init__(self, json_data):
         self.name = json_data["name"]
-        self.rating = json_data["rating"]
+        # self.rating = json_data["rating"]
         self.address = json_data["formatted_address"]
-        self.is_open = json_data["opening_hours"]["open_now"]
+        # self.is_open = json_data["opening_hours"]["open_now"]
 
     def print_place_information(self):
         print("--------------------------------------------------------------------")
         print("Name: " + self.name)
-        print("Google Reviews Rating: " + str(self.rating))
-        print("Address: " + self.address)
-        print("Is Currently Open For Business: " + str(self.is_open))
+        # print("Google Reviews Rating: " + str(self.rating))
+        # print("Address: " + self.address)
+        # print("Is Currently Open For Business: " + str(self.is_open))
+
+
+
 
 
